ac4397e06d393933bbc880224b3afc20133ac26c
niiad <samuelbrownai@gmail.com>
2024-03-21 08:08:17 +0000

    base.js: getting specific user

--- base.js
+++ base.js
@@ -14,6 +14,17 @@ app.get("/users", (req, res) => {
     res.json(users);
 });
 
+app.get("/users/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const user = users.find(user => user.id === id);
+
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).send("user not found");
+    }
+});
+
 app.listen(port, () => {
     console.log(`listening on port:${port}`)
 });
